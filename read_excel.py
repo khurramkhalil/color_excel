f90e2e44e9634ac85c9e2733480fce7408bd0e0b
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-01-31 03:28:35 -0800

    Documentedd the file with appropriate commenting.

--- read_excel.py
+++ read_excel.py
@@ -2,40 +2,28 @@ import pandas as pd
 import numpy as np
 from styleframe import StyleFrame, utils
 
-
+# only select cells whose character have red font, else np.nan
 def only_cells_with_red_text(cell):
     return cell if cell.style.font_color in {utils.colors.red, 'FFFF0000'} else np.nan
     # return cell if not (cell.style.font_color in {utils.colors.red, 'FFFF0000'}) else np.nan
 
-
+# Only select cells whose background is red, else np.nan
 def only_cells_with_red_background(cell):
     return cell if cell.style.bg_color in {utils.colors.red, 'FFFF0000'} else np.nan
     # return cell if not (cell.style.bg_color in {utils.colors.red, 'FFFF0000'}) else np.nan
 
 
+# Get all the sheets from the Excel file.
 sheet_names = pd.ExcelFile('test_excel.xlsx').sheet_names
 
+# Iterate through Excel sheets one by one
 for sheet in sheet_names:
+
+    # read current sheet from the Excel file
     sff = StyleFrame.read_excel('test_excel.xlsx', sheet_name=sheet, read_style=True, use_openpyxl_styles=False)
-    sf_2 = StyleFrame(sff.applymap(only_cells_with_red_text).dropna(axis=0, how='all'))
+
+    # call both functions one by one on all sheets and print the results
+    sf_1 = StyleFrame(sff.applymap(only_cells_with_red_text).dropna(axis=0, how='all'))
+    print(sf_1)
+    sf_2 = StyleFrame(sff.applymap(only_cells_with_red_background).dropna(axis=0, how='all'))
     print(sf_2)
-    sf_3 = StyleFrame(sff.applymap(only_cells_with_red_background).dropna(axis=0, how='all'))
-    print(sf_3)
-
-
-# sf = StyleFrame.read_excel('test_excel.xlsx', read_style=True)
-# print(sf)
-#
-#
-# sf1 = sf[[col for col in sf.columns if col.style.fill.fgColor.rgb in ('FFFFFFFF', utils.colors.white)]]
-#
-#
-
-# def empty_red_background_cells(cell):
-#     if cell.style.bg_color in {utils.colors.red, 'FFFF0000'}:
-#         cell.value = np.nan
-#     return cell
-#
-#
-# sf_1 = StyleFrame(sff.applymap(empty_red_background_cells))
-# print(sf_1)
